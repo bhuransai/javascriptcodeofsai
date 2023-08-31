d26d5358ea18cbb77de8b01f9112a13bb81d2bea
bhuransai <109899068+bhuransai@users.noreply.github.com>
2023-08-31 15:17:26 +0000

    name chages of file update the code

---
+++ 01_base/01_conversionsoperation.js
@@ -0,0 +1,56 @@
+// // //let score = 33
+
+// // //console.log(typeof score )
+
+// // //let valueInnumber = Number(score)
+// // //console.log(typeof score )
+
+// // //"33" => 33
+// // // "33abc"=>NaN
+// // // true => 1; false =>0
+
+// // //let isLoggedIn="sairaj"
+// // //let booleanIsLoggedIn = Boolean(isLoggedIn)
+// // //console.log( booleanIsLoggedIn);
+
+// // // let some = 33
+// // // let stringNumber = String(some)
+// // // console.log(stringNumber);
+// // // console.log(typeof stringNumber);
+// // // // 1 => true; 0=> flase
+// // // "" => flase
+// // //"sairaj"=>true
+
+
+// // //*********** operations ***************** *//
+
+// // let value  =3
+// // let negvalue = -value
+// // // console.log(negvalue)
+
+// // let str1 ="hello"
+// // let str2 = " sairaj"
+// // let str3= str1+str2
+// // console.log(str3);
+
+// // console.log("1"+2);
+// // console.log(1+ "2");
+// // console.log(1+"2")
+// console.log(+true);
+
+// let num1, num2,num3 
+
+// num1=num2=num3 = 2+2
+
+
+// let gamecounter = 100
+// gamecounter++;
+// console.log(gamecounter);
+console.log("2">1);
+console.log("02">1);
+console.log(null > 0);
+console.log(null == 0);
+console.log(null >= 0);
+///===
+
+console.log("2"===2);
